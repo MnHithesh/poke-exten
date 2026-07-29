d74f08d8855fc9c0de974cccd7fda5c500fdf518
Hithesh Nandeesh <m.n.hithesh26@gmail.com>
2026-07-29 16:42:07 +0530

    with token

--- poke-angular/poke-angular/extension/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { provideZonelessChangeDetection } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-
-bootstrapApplication(App, {
-  providers: [provideZonelessChangeDetection()],
-}).catch((err) => console.error(err));
